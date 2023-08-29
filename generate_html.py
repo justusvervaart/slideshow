3dcb597342f98e694b9216ef59d228bda6c2520f
Just Vervaart <35161717+justusvervaart@users.noreply.github.com>
2023-08-29 17:35:25 +0200

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -1,24 +1,67 @@
 import os
 
-def generate_html():
-    img_folder = "contents/fotos"
-    img_files = [f for f in os.listdir(img_folder) if f.endswith('.jpg') or f.endswith('.png')]
+# Definieer de locatie van de foto's en de output HTML-bestand
+foto_dir = 'contents/fotos'
+output_html = 'pagina.html'
 
-    with open("pagina.html", "w") as f:
-        f.write("<!DOCTYPE html>\n")
-        f.write("<html>\n")
-        f.write("<head>\n")
-        f.write("    <title>Foto Slideshow</title>\n")
-        f.write("</head>\n")
-        f.write("<body>\n")
-        f.write("    <div id='slideshow'>\n")
+# Start de HTML, CSS en JavaScript
+html_start = """<!DOCTYPE html>
+<html>
+<head>
+    <title>Foto Slideshow</title>
+    <style>
+        #slideshow {
+            width: 100%;
+            height: auto;
+            overflow: hidden;
+        }
+        .slide {
+            max-width: 100%;
+            max-height: 100vh;
+            object-fit: cover;
+            display: none;
+        }
+        .slide:first-of-type {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+<div id="slideshow">
+"""
 
-        for img_file in img_files:
-            f.write(f"        <img src='{img_folder}/{img_file}' alt='{img_file}'>\n")
+# Verkrijg alle foto-bestanden
+foto_files = [f for f in os.listdir(foto_dir) if os.path.isfile(os.path.join(foto_dir, f))]
+foto_files.sort()  # Sorteer de bestanden, indien nodig
 
-        f.write("    </div>\n")
-        f.write("</body>\n")
-        f.write("</html>\n")
+# Genereer de HTML voor de foto's
+foto_html = ''
+for foto in foto_files:
+    foto_path = os.path.join('contents/fotos', foto)
+    foto_html += f'    <img src="{foto_path}" class="slide">\n'
 
-if __name__ == "__main__":
-    generate_html()
+# Eindig de HTML, CSS en JavaScript
+html_end = """</div>
+<script>
+    let slideIndex = 0;
+    function showSlides() {
+        let slides = document.getElementsByClassName("slide");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 2000);
+    }
+    showSlides();
+</script>
+</body>
+</html>
+"""
+
+# Combineer alles en schrijf naar het output HTML-bestand
+with open(output_html, 'w') as f:
+    f.write(html_start + foto_html + html_end)
+
+print(f"'{output_html}' is succesvol gegenereerd.")
