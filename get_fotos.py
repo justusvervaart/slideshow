8d6024633d67445c92a4974a6fc594e39799c014
Just Vervaart <35161717+justusvervaart@users.noreply.github.com>
2023-08-29 21:23:09 +0200

    Update get_fotos.py

--- get_fotos.py
+++ get_fotos.py
@@ -1,16 +1,12 @@
 import imaplib
 import email
 import os
-from email.header import decode_header
 
 # Gmail IMAP instellingen van omgevingsvariabelen
 host = os.environ.get('EMAIL_HOST', 'imap.gmail.com')
 port = int(os.environ.get('EMAIL_PORT', 993))
 user = os.environ.get('EMAIL_USER', 'default_user@gmail.com')
 password = os.environ.get('EMAIL_PASSWORD', 'default_password')
-
-# Opslagdirectory
-storage_directory = '/contents/fotos'  # Pas dit aan naar je specifieke directory
 
 # Verbind met Gmail
 mail = imaplib.IMAP4_SSL(host, port)
@@ -21,29 +17,23 @@ mail.select('inbox')
 status, messages = mail.search(None, '(UNSEEN)')
 email_ids = messages[0].split()
 
-# Loop door elke e-mail en download bijlagen en inline afbeeldingen
+# Zorg ervoor dat de opslagmap bestaat
+opslag_map = 'contents/fotos'
+if not os.path.exists(opslag_map):
+    os.makedirs(opslag_map)
+
+# Loop door elke e-mail en download bijlagen
 for e_id in email_ids:
     resp, msg_data = mail.fetch(e_id, '(RFC822)')
     raw_email = msg_data[0][1]
     msg = email.message_from_bytes(raw_email)
     for part in msg.walk():
-        content_disposition = part.get("Content-Disposition")
-        content_type = part.get_content_type()
-
         if part.get_content_maintype() == 'multipart':
             continue
-        if content_disposition is None and 'image' not in content_type:
+        if part.get('Content-Disposition') is None:
             continue
-
         filename = part.get_filename()
-        if not filename:
-            # Als er geen bestandsnaam is, genereer een.
-            ext = mimetypes.guess_extension(part.get_content_type())
-            if not ext:
-                # Gebruik een standaard extensie
-                ext = '.bin'
-            filename = 'img-' + str(int(time.time())) + ext
-
-        filepath = os.path.join(storage_directory, filename)
-        with open(filepath, 'wb') as f:
-            f.write(part.get_payload(decode=True))
+        if filename:
+            filepath = os.path.join(opslag_map, filename)
+            with open(filepath, 'wb') as f:
+                f.write(part.get_payload(decode=True))
