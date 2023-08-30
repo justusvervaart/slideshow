a4b1f1ee15940f6dcfd4bdbcb0862604deafc41b
Just Vervaart <35161717+justusvervaart@users.noreply.github.com>
2023-08-30 08:23:46 +0200

    Create delete_fotos.py

---
+++ delete_fotos.py
@@ -0,0 +1,20 @@
+import os
+import glob
+
+# Definieer de directory waarin de foto's staan
+storage_directory = 'contents/fotos'
+
+# Controleer of de map bestaat
+if not os.path.exists(storage_directory):
+    print("De map bestaat niet.")
+else:
+    # Lijst alle bestanden in de map
+    files = glob.glob(f"{storage_directory}/*")
+
+    # Sorteer de bestanden op aanmaakdatum
+    files.sort(key=os.path.getctime, reverse=True)
+
+    # Behoud alleen de 30 meest recente
+    for file in files[3:]:
+        os.remove(file)
+        print(f"Verwijderd: {file}")
