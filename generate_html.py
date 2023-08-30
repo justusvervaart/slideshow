47c34ee4d8b4be958715f58beb1df89b88d24867
Just Vervaart <35161717+justusvervaart@users.noreply.github.com>
2023-08-30 16:59:59 +0200

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -1,7 +1,23 @@
 import os
 import glob
+import csv
+
+def read_captions():
+    captions = {}
+    try:
+        with open('contents/captions.csv', mode='r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                filename, caption = row
+                captions[filename] = caption
+    except FileNotFoundError:
+        print("Geen captions.csv gevonden.")
+    return captions
 
 def generate_html():
+    # Lees de onderschriften uit het CSV-bestand
+    captions = read_captions()
+
     # Vind alle bestanden met de gegeven extensies
     extensions = ['jpg', 'jpeg', 'JPG', 'JPEG']
     image_files = []
@@ -12,50 +28,31 @@ def generate_html():
     image_files.sort()
 
     # Begin met het genereren van HTML
-    html_content = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Foto Slideshow</title>
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <style>
-        /* Zorg ervoor dat de afbeelding altijd op het scherm past */
-        body {
-            background-color: black;
-            margin: 0;
-            padding: 0;
-        }
-        #slideshow img {
-            max-width: 100%;
-            max-height: 100vh;
-            object-fit: contain;
-            margin: auto;
-            display: block;
-        }
-    </style>
-    <meta http-equiv="refresh" content="3600"> // ververs de pagina ieder uur 
-</head>
-<body>
+    # ... (geen wijzigingen hier)
 
-<div id="slideshow">
-"""
-
-    # Voeg afbeeldingen toe aan HTML
+    # Voeg afbeeldingen en onderschriften toe aan HTML
     for image_file in image_files:
         rel_path = os.path.relpath(image_file, start='.')
-        html_content += f'    <img src="{rel_path}" alt="{rel_path}" style="display:none;">\n'
+        filename = os.path.basename(image_file)
+        caption = captions.get(filename, '')  # Haal het onderschrift op als het bestaat
+        html_content += f'    <div class="slide" style="display:none;">\n'
+        html_content += f'        <img src="{rel_path}" alt="{rel_path}">\n'
+        html_content += f'        <p>{caption}</p>\n'
+        html_content += f'    </div>\n'
 
     # Voeg script toe voor het wisselen van de foto's
+    # ... (hier moeten wat kleine wijzigingen worden aangebracht)
+
     html_content += """
 </div>
 
 <script>
     var index = 0;
     function showNextImage() {
-        var images = document.querySelectorAll("#slideshow img");
-        images[index].style.display = "none";
-        index = (index + 1) % images.length;
-        images[index].style.display = "block";
+        var slides = document.querySelectorAll(".slide");
+        slides[index].style.display = "none";
+        index = (index + 1) % slides.length;
+        slides[index].style.display = "block";
     }
     setInterval(showNextImage, 300000); // iedere 5 minuten een nieuwe foto om mijn moeder niet teveel te verwarren
     // Toon de eerste afbeelding
